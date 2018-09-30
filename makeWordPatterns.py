af10f4aa84ef60170bc0974df7d2de495c6da630
ranand5 <hitanand4@gmail.com>
2018-09-30 18:23:14 +0530

    adding the file that can hack the subsstitution cipher

--- makeWordPatterns.py
+++ makeWordPatterns.py
@@ -1,16 +1,13 @@
-# Makes the wordPatterns.py File
-# http://inventwithpython.com/hacking (BSD Licensed)
-
 # Creates wordPatterns.py based on the words in our dictionary
-# text file, dictionary.txt. (Download this file from
-# http://invpy.com/dictionary.txt)
 
-import pprint
+import pprint # pretty printing values whixhis useful for printing dictionary and list values in the list
 
 
 def getWordPattern(word):
     # Returns a string of the pattern form of the given word.
     # e.g. '0.1.2.3.4.1.2.3.5.6' for 'DUSTBUSTER'
+    # it is mucch easier to start woth the blank list instead of blank string, and then use append()
+    # list method instead for string cocatenation
     word = word.upper()
     nextNum = 0
     letterNums = {}
